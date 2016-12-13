8fcdb25577b184d7625a5b8c1f9a080eddaba696
Karla Hugo <karlabrandon817@gmail.com>
2016-12-13 11:53:11 -0600

    final commit

--- solution.sql
+++ solution.sql
@@ -5,7 +5,7 @@ SELECT * FROM syntax_practice WHERE city='chicago';
 -- 2. Get all users with usernames that contain the letter a.
 SELECT * FROM syntax_practice WHERE username LIKE '%a%';
 -- 3. The bank is giving a new customer bonus! Update all records with an account balance of 0.00 and a transactions_attempted of 0. Give them a new account balance of 10.00.
-UPDATE syntax_practice SET account_balance='10.00' WHERE account_balance='0.00';
+UPDATE syntax_practice SET account_balance='10.00' WHERE account_balance='0.00' and transactions_attempted='0';
 -- 4. Select all users that have attempted 9 or more transactions.
 SELECT * FROM syntax_practice WHERE transactions_attempted >='9';
 -- 5. Get the username and account balance of the 3 users with the highest balances, sort highest to lowest balance. NOTE: Research LIMIT
